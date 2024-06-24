01f03bf25001bb109197c4619d157e46a886662e
GECS12 <traducao.gc@gmail.com>
2024-06-24 20:54:22 +0100

    Lots of updates

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -8,6 +8,11 @@ import os
 import certifi
 import hashlib
 from aux_functions.db_functions import *
+import os
+import sys
+
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(base_dir)
 
 load_dotenv()
 
